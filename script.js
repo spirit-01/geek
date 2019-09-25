2f2302f52d828689dd6ab9934509f756937f49c1
Kirill <postbykirill@yandex.ru>
2019-09-25 14:39:15 +0300

    ООП по методичке

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 
-const goods = [
-    { title: 'футболка', price: 500 },
-    { title: 'носки', price: 50 },
-    { title: 'трусы', price: 350 },
-    { title: 'джинсы', price: 2000 },
-    {  price: 200 },
-    { title: 'куртка' }
-  ];
-  
-  const renderGoodsItem = ({title="нет товара",price="цена уточняется"}) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-  };
-  
-  const renderGoodsList = list => {
-    let goodsList = list.map(renderGoodsItem);
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+ 
+  class GoodsItem {
+    constructor(title,price) {
+      this.title=title;
+      this.price=price;
+    }
+    render () {
+      return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
   }
-  
-  renderGoodsList(goods);
-  
+ 
+  class GoodsList{
+    constructor() {
+      this.goods=[];
+    }
+    fetchGoods() {
+      this.goods=[
+        { title: 'футболка', price: 500 },
+        { title: 'носки', price: 50 },
+        { title: 'трусы', price: 350 },
+        { title: 'джинсы', price: 2000 },
+      ];
+    }
+    render() {
+     return this.goods.map(good=>new GoodsItem(good.title,good.price).render()).join('');
+    }
+  }
+
+  const list= new GoodsList();
+  list.fetchGoods();
+  document.querySelector('.goods-list').innerHTML=list.render();
